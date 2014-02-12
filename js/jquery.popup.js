2c09d6be739a33fcde4094b37472da66981f2e1e
Hiroaki Nakamura <hnakamur@gmail.com>
2014-02-12 10:45:36 +0900

    Remove event handlers when a popup is closed.

--- js/jquery.popup.js
+++ js/jquery.popup.js
@@ -1,35 +1,45 @@
 (function($, undefined) {
-  $.fn.popup = function(options) {
-    if (options === "open") {
-      options = this.data("options");
-      this.show();
-    } else if (options === "close") {
-      this.hide();
-    } else {
-      this.data("options", options);
+  $.fn.popup = function(command, options) {
+    var el = this, doc = $(document);
+    if (command === "open") {
       if (options.top !== undefined || options.left !== undefined) {
         var pos = {};
-        if (options.top !== undefined)
-          pos.top = options.top;
-        if (options.left !== undefined)
-          pos.left = options.left;
+        pos.top = options.top || 0;
+        pos.left = options.left || 0;
         this.css(pos);
       }
 
-      this.click(function(e) {
+      function popupClickHandler(e) {
         e.stopPropagation();
-      });
+      }
 
-      var el = this;
-      $(document).click(function(e) {
+      function docClickHandler(e) {
         el.popup("close");
-      });
+      }
 
-      $(document).keydown(function(e) {
+      function docKeydownHandler(e) {
         if (e.which === 27) { // ESC
           el.popup("close");
         }
+      }
+
+      el.on("click", popupClickHandler);
+      doc.on("click", docClickHandler);
+      doc.on("keydown", docKeydownHandler);
+      el.data("popup", {
+        handlers: {
+          popupClick: popupClickHandler,
+          docClick: docClickHandler,
+          docKeydown: docKeydownHandler
+        }
       });
+      el.show();
+    } else if (command === "close") {
+      el.hide();
+      var handlers = el.data("popup").handlers;
+      el.off("click", handlers.popupClick);
+      doc.off("click", handlers.docClick);
+      doc.off("keydown", handlers.docKeydown);
     }
   };
 }(jQuery));
